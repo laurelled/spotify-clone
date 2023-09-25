7292a28cc43e4f751c11595d863764de76d085b1
laurelled <bfn.lorenzo@gmail.com>
2023-09-25 08:59:55 +0200

    config eslint and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      "black-main": "#121212",
+      "spotify-green": "#1FDF64",
+      "black-secondary": "#242424",
+      "white": "#ffffff",
+      "subtitle": "#949494"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
