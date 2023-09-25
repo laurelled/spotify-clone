af8d7659ead674271c8933031274cdcef55c06d4
laurelled <bfn.lorenzo@gmail.com>
2023-09-25 14:02:19 +0200

    change configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [ "./src/**/*.{html,ts}"],
-  theme: {
-    colors: {
-      "black-main": "#121212",
-      "spotify-green": "#1FDF64",
-      "black-secondary": "#242424",
-      "white": "#ffffff",
-      "subtitle": "#949494"
-    },
-    extend: {},
-  },
-  plugins: [],
-}
+import colors from "tailwindcss/colors";
 
+export const content = ["./src/**/*.{html,ts}"];
+export const theme = {
+  colors: {
+    "black-main": "#121212",
+    "spotify-green": "#1FDF64",
+    "black-secondary": "#242424",
+    subtitle: "#949494",
+    transparent: "transparent",
+    current: "currentColor",
+    black: colors.black,
+    white: colors.white,
+    gray: colors.gray,
+    emerald: colors.emerald,
+    indigo: colors.indigo,
+    yellow: colors.yellow,
+  },
+  extend: {},
+};
+export const plugins = [];
